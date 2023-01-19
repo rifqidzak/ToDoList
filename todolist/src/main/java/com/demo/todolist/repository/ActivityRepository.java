b7b59e4fa3beda582ee97a90125754685e82ab62
rifqidzak <rifdzak@gmail.com>
2023-01-19 14:44:23 +0700

    fixing query scheduler

--- todolist/src/main/java/com/demo/todolist/repository/ActivityRepository.java
+++ todolist/src/main/java/com/demo/todolist/repository/ActivityRepository.java
@@ -12,7 +12,7 @@ public interface ActivityRepository extends JpaRepository<Activity, String>{
     @Query(value = "select * from t_activity inner join t_category ON t_activity.category_id = t_category.id where t_activity.is_active = true", nativeQuery = true)
 	List<Activity> getAllActiveSchedule();
 
-    @Query(value = "select * from t_activity inner join t_category ON t_activity.category_id = t_category.id where end_time < now() AND t_activity.is_active = true", nativeQuery = true)
+    @Query(value = "select * from t_activity left join t_category ON t_activity.category_id = t_category.id where end_time < now() AND t_activity.is_active = true", nativeQuery = true)
     List<Activity> getAllExpiredByTimeForScheduler();
     
     @Query(value = "select * from t_activity inner join t_category ON t_activity.category_id = t_category.id where t_activity.is_active = false", nativeQuery = true)
